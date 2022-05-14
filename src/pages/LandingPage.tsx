56658eeee75c49ea6196dff458f2c499193de522
La Prise <gaurondorian@gmail.com>
2022-05-14 17:03:22 +0200

    Finish header

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import Header from "../common/components/Header"
+
+
+const LandingPage = () => {
+
+
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default LandingPage;
