38fb171893ca64cdec21b236a4fee187c4d720f3
Gauron Dorian <gaurondorian@gmail.com>
2022-05-17 12:31:23 +0200

    Parcourez l'univers
    Finish parcourez l'univers

--- src/common/components/Button/ButtonViolet.tsx
+++ src/common/components/Button/ButtonViolet.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import IButton from '../../../interfaces/IButton';
 
-const ButtonViolet = ({link, text} : IButton) =>{
+const ButtonViolet = ({link, text, className} : IButton) =>{
     return(
         <Link to={link}>
-            <li> <button type="button" className='button_purple'>{text}</button></li>    
+            <li> <button type="button" className={'button_purple ' + className}>{text}</button></li>    
         </Link>
     )
 }
