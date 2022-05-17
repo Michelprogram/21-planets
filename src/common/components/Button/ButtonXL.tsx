d9e2a1a1066dedf9081e28c3c6ddda346284778d
Gauron Dorian <gaurondorian@gmail.com>
2022-05-17 11:43:22 +0200

    Interface
    New interface for button type

--- src/common/components/Button/ButtonXL.tsx
+++ src/common/components/Button/ButtonXL.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import IButton from '../../../interfaces/IButton';
 
-const ButtonXL = (props:any) =>{
+const ButtonXL = ({link, text} : IButton) =>{
     return(
-        <Link to={props.link}>
-            <li> <button type="button" className='button_XL button_blue'>{props.text}</button></li>    
+        <Link to={link}>
+            <li> <button type="button" className='button_XL button_blue'>{text}</button></li>    
         </Link>
     )
 }
---
+++ src/interfaces/IButton.tsx
@@ -0,0 +1,4 @@
+export default interface IButton{
+    link: string,
+    text: string
+}
