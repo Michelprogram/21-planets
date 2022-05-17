d344328e9e3a0047cfbec951ea0522807544b1d4
dcaruso8 <daryltron@gmail.com>
2022-05-17 15:08:41 +0200

    prix attractifs component

---
+++ src/common/components/PrixAttractifs.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import PriceCard from './Card/PriceCard';
+
+const PrixAttractifs = () => {
+    
+    return (
+        <div className='prix_attractifs'>
+            <h2>Des prix attractifs</h2>
+            <br />
+            <h4><q>Qui aurait cru qu’on pouvait aller si haut en dépensant si peu ?</q> - Gaspard, probablement</h4>
+
+            <br /><br />
+            <PriceCard logo="redGiants.svg" price="50 000 €" title="Le pack de deux géantes rouges" elements={['Aldébaran (Taureau)','Beltégeuse (Orion)']} link="/login"/>
+            <PriceCard logo="saturn.svg" price="100 000 €" title="Saturne et ses anneaux" elements={['Des centaines de coloris disponibles','Mode holographique inclu']} link="/login"/>
+            <PriceCard logo="blackHole.svg" price="5 200 000 000 €" title="3 trous noirs (2 + 1 gratuit)" elements={['Q0906+6930','Cygnus X-1','Le troisième au choix parmi des centaines de modèles']} link="/login"/>
+        </div>
+    );
+};
+export default PrixAttractifs;
