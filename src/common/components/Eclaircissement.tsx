390c69a5439f01a9bd5e77389242459fccf7bd82
Gauron Dorian <gaurondorian@gmail.com>
2022-05-17 17:01:28 +0200

    Finish
    Component éclaircissement

---
+++ src/common/components/Eclaircissement.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Eclaircissement = () => {
+    return (
+        <div className='container-eclaircissement'>
+            <p className="title">☄️ Besoin d’un éclaircissement ?</p>
+            <div className="container-questions">
+                <p>Votre planète ne répond plus ?</p>
+                <p>Vos trous noirs ne sont pas noirs ?</p>
+                <p>Un problème conçernant la brillance de vos étoiles ?</p>
+                <p>Vous ne trouvez plus votre planète ?</p>
+                <p>Vous voulez en savoir plus sur la garantie vol/casse de votre planète ?</p>
+                <p>Vous soupçonnez vos trous noirs d’avoir avalés votre mère (ou autre...) ?</p>
+            </div>
+        </div>
+    );
+};
+
+export default Eclaircissement;
