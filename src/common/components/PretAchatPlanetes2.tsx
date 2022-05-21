93c372b4ba69180f6b997545244ec8890ad00561
dcaruso8 <daryltron@gmail.com>
2022-05-21 23:07:26 +0200

    changement des boutons login/shop en fonction de l'utilisateur

--- src/common/components/PretAchatPlanetes2.tsx
+++ src/common/components/PretAchatPlanetes2.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
 import setClassName from '../../utils/ClassName';
 import ButtonBlue from './Button/ButtonBlue';
 import ButtonViolet from './Button/ButtonViolet';
+import { useUser } from '../../context/UserContext';
 
 const PretAchatPlanetes2 = ({className=''}) =>{
+    const user:string = useUser();
+
     return(
         <div className={setClassName("pret_achat_2_container",className)}>
            <div className='title'>
-            <h3>Prêt à acheter des planètes ?</h3>
-            <p>Connectez-vous</p>
+            <h3>Prêt(e) à acheter des planètes ?</h3>
+            <p>Demandez-nous la lune...</p>
            </div>
             
             <div className='buttons'>
-                <ButtonBlue text="Acheter" link="/shop"/>
-                <ButtonViolet text="Connexion" link="/login"/>
+                {user!='' ?  <ButtonBlue text="Acheter" link="/shop"/> : <ButtonViolet text="Connexion" link="/login"/>}
             </div>
         </div>
     );
