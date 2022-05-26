98bfad86560ab455ee63188946f51054bbd7f671
dcaruso8 <daryltron@gmail.com>
2022-05-26 12:21:19 +0200

    récupération données asteroides ok

--- src/api/asteroids.ts
+++ src/api/asteroids.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import IAsteroide from "../interfaces/IAsteroide";
 
 const URI: string = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY";
-let data: Array<IAsteroide> = [];
+let data: Array<any> = [];
 
 const fetchAsteroides = async () => {
   if (data.length > 0) return data;
@@ -10,7 +10,10 @@ const fetchAsteroides = async () => {
 
   try {
     const request = await axios.get(URI);
-    request.data.forEach((element: IAsteroide) => data.push(element));
+    request.data.near_earth_objects.map((element: any) => data.push(element));
+    
+    console.log(data);
+
     return data;
   } catch (err) {
     console.warn("Problème lors du fetch à l'api des asteroides : ", err);
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from 'react';
+import fetchAsteroides from '../api/asteroids';
 import Description from '../common/components/Description';
-import { SelectedItemProvider, useSelectedItem, useUpdateSelectedItem } from '../context/SelectedItemContext';
+import { useSelectedItem } from '../context/SelectedItemContext';
 
 const Detail = () => {
     const selectedItem = useSelectedItem();
+    const [data, setData] = useState<Array<any>>([])
+
+    useEffect(() => {
+        fetchAsteroides().then( (asteroides:Array<any>) => setData(asteroides))
+    }, []);
 
     return (
         <div>
@@ -10,5 +17,7 @@ const Detail = () => {
         </div>
     );
 };
+
+//data.map((el:INews, index:number)=><CardNews key={index}{...el}/>)
 
 export default Detail;
