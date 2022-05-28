792a85b26450c25d7e2d08b522e3a845f41b395f
Michelprogram <gaurondorian@gmail.com>
2022-05-28 13:12:39 +0200

    update

--- src/common/components/Card/Articles.tsx
+++ src/common/components/Card/Articles.tsx
@@ -1,32 +1,30 @@
-import React, { useState } from 'react';
-import IFakeData from '../../../interfaces/IFakeData';
+import React, { useState } from "react";
+import IFakeData from "../../../interfaces/IFakeData";
 
-import {svgIcones as PlanetsSVG} from "../../../constants/Images"
-import { random } from '../../../utils/Random';
+import { svgIcones as PlanetsSVG, Trash } from "../../../constants/Images";
+import { random } from "../../../utils/Random";
 
-import usePanier from '../../hooks/panier';
+import usePanier from "../../hooks/panier";
 
-import { Trash } from '../../../constants/Images';
-import { ReadablePrice } from '../../../utils/String';
+import { ReadablePrice } from "../../../utils/String";
 
-const Articles = ({id, planete}: IFakeData) => {
+const Articles = ({ id, planete }: IFakeData) => {
+  const [icons, _] = useState<Array<string>>(PlanetsSVG);
 
-    const [icons, _] = useState<Array<string>>(PlanetsSVG)
+  const { remove } = usePanier();
 
-    const {remove} = usePanier();
+  const randomIcon = (): string => {
+    return icons[random(0, icons.length)];
+  };
 
-    const randomIcon = () :string =>{
-        return icons[random(0, icons.length)]
-    }
-
-    return (
-        <div className='container-article'>
-            <img src={randomIcon()} alt="" className="icon" />
-            <p className='name'>{planete.name}</p>
-            <p className='amount'>{ReadablePrice(planete.amount)} $</p>
-            <img className="trash" src={Trash} alt="" onClick={ () => remove(id) }/>
-        </div>
-    );
+  return (
+    <div className="container-article">
+      <img src={randomIcon()} alt="" className="icon" />
+      <p className="name">{planete.name}</p>
+      <p className="amount">{ReadablePrice(planete.amount)} $</p>
+      <img className="trash" src={Trash} alt="" onClick={() => remove(id)} />
+    </div>
+  );
 };
 
 export default Articles;
