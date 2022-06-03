c1b2f616cb9f138c9256196e68ea586e0c42200a
Michelprogram <gaurondorian@gmail.com>
2022-06-03 19:08:47 +0200

    waiting for fetch

--- src/common/components/Shop.tsx
+++ src/common/components/Shop.tsx
@@ -4,11 +4,23 @@ import fetchExoplanete from "../../api/Exoplanete";
 import IExoplanete from "../../interfaces/IExoplanete";
 import Planets from "../../common/components/Card/Planets";
 import fetchComete from "../../api/Comete";
+import { Waiting } from "../../constants/Images";
 
 const Shop = ({ title }: any) => {
   const [data, setData] = useState<Array<any>>([]);
 
-  const whichCard = () => {
+  const waitingFetch = (): JSX.Element => {
+    return (
+      <div className="waiting-container">
+        <img src={Waiting} alt="" />
+        <p>
+          Patientez pendant que nous cherchons vos données à travers le cosmos
+        </p>
+      </div>
+    );
+  };
+
+  const whichCard = (): Array<JSX.Element> => {
     let res: Array<JSX.Element> = [];
 
     switch (title) {
@@ -45,7 +57,11 @@ const Shop = ({ title }: any) => {
     whichData();
   }, []);
 
-  return <div className="container-shop-items">{whichCard()}</div>;
+  return (
+    <div className="container-shop-items">
+      {data.length == 0 ? waitingFetch() : whichCard()}
+    </div>
+  );
 };
 
 export default Shop;
