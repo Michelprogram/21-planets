eb519dbdd2184b08d94087b695856ed838135aab
Michelprogram <gaurondorian@gmail.com>
2022-06-04 17:58:02 +0200

    little fix

--- src/common/components/Card/AsteroideCard.tsx
+++ src/common/components/Card/AsteroideCard.tsx
@@ -1,34 +1,33 @@
-import {Link} from "react-router-dom";
-import { useUpdateSelectedItem } from '../../../context/SelectedItemContext';
+import { Link } from "react-router-dom";
+import { useUpdateSelectedItem } from "../../../context/SelectedItemContext";
 import IAsteroid from "../../../interfaces/IAsteroide";
 
-const AsteroideCard = ({id, asteroide}:IAsteroid) => {
+const AsteroideCard = ({ id, asteroide }: any) => {
+  const updateSelectedItem = useUpdateSelectedItem();
+  function update() {
+    updateSelectedItem(asteroide);
+  }
 
-    const updateSelectedItem = useUpdateSelectedItem();
-    function update(){
-        updateSelectedItem(asteroide);
-    }
+  const getColor = () => {
+    return { backgroundColor: asteroide.color };
+  };
 
-    const getColor = () =>{
-        return {backgroundColor: asteroide.color}
-    }
-    
-    return (
-        <div className='container-card-planete'>
-            <div className="top-side" style={getColor()}>
-                <img src={asteroide.icon} className='round'/>   
-            </div>
-            <div className="description">
-                <div>
-                    <p className="title">{asteroide.name_limited}</p>
+  return (
+    <div className="container-card-planete">
+      <div className="top-side" style={getColor()}>
+        <img src={asteroide.icon} className="round" />
+      </div>
+      <div className="description">
+        <div>
+          <p className="title">{asteroide.name_limited}</p>
 
-                    <Link to="/detail" onClick={update}>
-                        <p>Voir plus &gt;</p>
-                    </Link>
-                </div>
-            </div>
+          <Link to="/detail" onClick={update}>
+            <p>Voir plus &gt;</p>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AsteroideCard;
