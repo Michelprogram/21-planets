6c2b28a78c83393eaf0db421f2cb8494d30a0127
Michelprogram <gaurondorian@gmail.com>
2022-06-07 18:21:12 +0200

    search bar

--- src/common/components/Shop.tsx
+++ src/common/components/Shop.tsx
@@ -2,10 +2,36 @@ import Item from "./Card/Item";
 import { Waiting } from "../../constants/Images";
 import useData from "../hooks/Data";
 import IData from "../../interfaces/IData";
-import {getColor} from "../../constants/ColorPalette";
+import { getColor } from "../../constants/ColorPalette";
+import { useState } from "react";
 
 const Shop = ({ title }: any) => {
   const { apiData, filterByType } = useData();
+  const [search, setSearch] = useState<string>("");
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let lowerCase = e.target.value.toLowerCase();
+    setSearch(lowerCase);
+  };
+
+  const filterData = () => {
+    return filterByType(title)
+      .filter((el: IData) => {
+        if (search == "") return el;
+        return el.name.toLowerCase().includes(search);
+      })
+      .map((el: IData, index: number) => {
+        return (
+          <Item
+            key={index}
+            name={el.name}
+            image={el.image}
+            color={getColor(title)}
+            id={el.id}
+          />
+        );
+      });
+  };
 
   if (!apiData) {
     return (
@@ -22,13 +48,9 @@ const Shop = ({ title }: any) => {
   }
 
   return (
-    <div className="container-shop-items">
-      { 
-      filterByType(title).map((el: IData, index: number) => {
-        return <Item key={index} name={el.name} image={el.image} color={getColor(title)} id={el.id} />;
-      })
-      
-      }
+    <div>
+      <input type="text" onChange={inputHandler} />
+      <div className="container-shop-items">{filterData()}</div>
     </div>
   );
 };
